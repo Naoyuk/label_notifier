7a204a5a418fc0f05ad9010aa9e53151806ec763
Naoyuki Ishida <39.ishida@gmail.com>
2024-07-16 07:56:48 -0700

    feat: basic CRUD for Label

--- Gemfile
+++ Gemfile
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-ruby "3.2.2"
+ruby '3.2.2'
 
-gem "rails", "~> 7.1.3", ">= 7.1.3.4"
-gem "sprockets-rails"
-gem "pg"
-gem "puma"
-gem "importmap-rails"
-gem "turbo-rails"
-gem "stimulus-rails"
-gem "jbuilder"
-gem "redis"
-gem "tzinfo-data", platforms: %i[ windows jruby ]
-gem "bootsnap", require: false
+gem 'bootsnap', require: false
+gem 'importmap-rails'
+gem 'jbuilder'
+gem 'pg'
+gem 'puma'
+gem 'rails', '~> 7.1.3', '>= 7.1.3.4'
+gem 'redis'
+gem 'sprockets-rails'
+gem 'stimulus-rails'
+gem 'turbo-rails'
+gem 'tzinfo-data', platforms: %i[windows jruby]
 
 group :development, :test do
-  gem "debug", platforms: %i[ mri windows ]
+  gem 'debug', platforms: %i[mri windows]
   gem 'factory_bot_rails'
   gem 'rspec-rails'
   gem 'rubocop'
@@ -25,11 +25,10 @@ group :development, :test do
 end
 
 group :development do
-  gem "web-console"
+  gem 'web-console'
 end
 
 group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
 end
-
--- spec/requests/labels_spec.rb
+++ spec/requests/labels_spec.rb
@@ -1,83 +1,82 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "/labels", type: :request do
-  
+RSpec.describe '/labels', type: :request do
   let(:valid_attributes) { FactoryBot.attributes_for(:label) }
   let(:invalid_attributes) { FactoryBot.attributes_for(:label, title: nil) }
 
-  describe "GET /index" do
-    it "renders a successful response" do
+  describe 'GET /index' do
+    it 'renders a successful response' do
       Label.create! valid_attributes
       get labels_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /show" do
-    it "renders a successful response" do
+  describe 'GET /show' do
+    it 'renders a successful response' do
       label = Label.create! valid_attributes
       get label_url(label)
       expect(response).to be_successful
     end
   end
 
-  describe "GET /new" do
-    it "renders a successful response" do
+  describe 'GET /new' do
+    it 'renders a successful response' do
       get new_label_url
       expect(response).to be_successful
     end
   end
 
-  describe "GET /edit" do
-    it "renders a successful response" do
+  describe 'GET /edit' do
+    it 'renders a successful response' do
       label = Label.create! valid_attributes
       get edit_label_url(label)
       expect(response).to be_successful
     end
   end
 
-  describe "POST /create" do
-    context "with valid parameters" do
-      it "creates a new Label" do
-        expect {
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'creates a new Label' do
+        expect do
           post labels_url, params: { label: valid_attributes }
-        }.to change(Label, :count).by(1)
+        end.to change(Label, :count).by(1)
       end
 
-      it "redirects to the created label" do
+      it 'redirects to the created label' do
         post labels_url, params: { label: valid_attributes }
         expect(response).to redirect_to(label_url(Label.last))
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Label" do
-        expect {
+    context 'with invalid parameters' do
+      it 'does not create a new Label' do
+        expect do
           post labels_url, params: { label: invalid_attributes }
-        }.to change(Label, :count).by(0)
+        end.to change(Label, :count).by(0)
       end
 
-    
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post labels_url, params: { label: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
     end
   end
 
-  describe "PATCH /update" do
-    context "with valid parameters" do
+  describe 'PATCH /update' do
+    context 'with valid parameters' do
       let(:new_attributes) { FactoryBot.attributes_for(:label, title: 'updated_title') }
 
-      it "updates the requested label" do
+      it 'updates the requested label' do
         label = Label.create! valid_attributes
         patch label_url(label), params: { label: new_attributes }
         label.reload
         expect(label.title).to eq 'updated_title'
       end
 
-      it "redirects to the label" do
+      it 'redirects to the label' do
         label = Label.create! valid_attributes
         patch label_url(label), params: { label: new_attributes }
         label.reload
@@ -85,26 +84,24 @@ RSpec.describe "/labels", type: :request do
       end
     end
 
-    context "with invalid parameters" do
-    
+    context 'with invalid parameters' do
       it "renders a response with 422 status (i.e. to display the 'edit' template)" do
         label = Label.create! valid_attributes
         patch label_url(label), params: { label: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
       end
-    
     end
   end
 
-  describe "DELETE /destroy" do
-    it "destroys the requested label" do
+  describe 'DELETE /destroy' do
+    it 'destroys the requested label' do
       label = Label.create! valid_attributes
-      expect {
+      expect do
         delete label_url(label)
-      }.to change(Label, :count).by(-1)
+      end.to change(Label, :count).by(-1)
     end
 
-    it "redirects to the labels list" do
+    it 'redirects to the labels list' do
       label = Label.create! valid_attributes
       delete label_url(label)
       expect(response).to redirect_to(labels_url)
